f64feb2f957547d0f30d94ed3c4056c1fe0ccd15
Matt York <myork@cs.ua.edu>
2015-02-03 09:36:04 -0600

    - Removed gear spec linting and creation. Renamed Manage page to Manage Gradle Dependencies

--- GradleBuddy/src/Actions/ManageGradleAction.java
+++ GradleBuddy/src/Actions/ManageGradleAction.java
@@ -1,19 +1,20 @@
 package Actions;
 
-import Forms.CreateGearForm;
-import Forms.ManageAndroidGearsForm;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import Forms.ManageAndroidGearsForm;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
- * Created by matthewyork on 4/2/14.
+ * Created by matthewyork on 3/31/14.
  */
-public class CreateGearAction extends AnAction {
+public class ManageGradleAction extends AnAction {
     public void actionPerformed(AnActionEvent e) {
-        JFrame frame = new JFrame("Create Android Gear");
-        frame.setContentPane(new CreateGearForm().MasterPanel);
+        JFrame frame = new JFrame("Manage Android Gears");
+        frame.setContentPane(new ManageAndroidGearsForm().MasterPanel);
+        frame.setResizable(false);
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
